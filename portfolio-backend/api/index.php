3cc486b1830a8c2969c48335b322bc62f6bad614
Ruslan <muhamedruslan392@gmail.com>
2026-03-13 17:44:52 +0530

    Add Laravel handler

--- portfolio-backend/api/index.php
+++ portfolio-backend/api/index.php
@@ -1,2 +1,37 @@
 <?php
-echo "PHP works!";
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$appPath = '/var/task/user';
+$tmpPath = '/tmp/laravel';
+
+$dirs = [
+    $tmpPath . '/storage/logs',
+    $tmpPath . '/storage/framework/cache/data',
+    $tmpPath . '/storage/framework/sessions',
+    $tmpPath . '/storage/framework/views',
+    $tmpPath . '/bootstrap/cache',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0775, true);
+    }
+}
+
+require $appPath . '/vendor/autoload.php';
+$app = require_once $appPath . '/bootstrap/app.php';
+$app->useStoragePath($tmpPath . '/storage');
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$request = Illuminate\Http\Request::capture();
+
+try {
+    $response = $kernel->handle($request);
+    $response->send();
+    $kernel->terminate($request, $response);
+} catch(\Throwable $e) {
+    echo "Error: " . $e->getMessage();
+    echo "<br>File: " . $e->getFile();
+    echo "<br>Line: " . $e->getLine();
+}
